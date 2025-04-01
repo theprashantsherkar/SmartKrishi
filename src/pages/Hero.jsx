52ce0c9fff0c2e1cdf0d9b23081bbef8e1bb107f
Prashant <prashantsherkar7515400@gmail.com>
2025-04-02 01:46:44 +0530

    adding a image slider

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {ImagesSlider} from '@/components/ui/images-slider';
 
 function Hero() {
+
+const images = ['/image.jpg', 'scene1.webp', 'scene2.webp', 'scene3.webp'];
+
   return (
-      <header className="flex-grow flex items-center container mx-auto px-6 py-16">
-          <div className="w-1/2">
+      <header className="flex-grow flex items-center container mx-auto px-6 py-16 h-[90vh]">
+          <div className="w-1/2 h-fit">
               <h2 className="text-5xl font-bold text-green-900 mb-6">
                   Precision Agriculture, Powered by Technology
               </h2>
@@ -20,12 +24,8 @@ function Hero() {
                   </button>
               </div>
           </div>
-          <div className="w-1/2">
-              <img
-                  src="image.jpg"
-                  alt="SmartKrishi Dashboard"
-                  className="rounded-lg shadow-xl"
-              />
+          <div className="w-1/2 h-3/4 rounded-xl overflow-hidden">
+              <ImagesSlider images={images}/>
           </div>
       </header>
   )
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 
 function Signin() {
   return (
-    <div className=' h-screen flex justify-center items-center'>
-          <Form label={"SignUp"} isLogin={false}  />
+    <div className=' h-screen flex justify-center items-center flex-col gap-5'>
+      <div className='flex items-center justify-center'>
+        <img src="/logo.png" alt="SmartKrishi" />
+        <h1 className='text-3xl font-bold'>SmartKrishi</h1>
+      </div>
+          <Form label={"Sign Up"} isLogin={false}  />
     </div>
   )
 }

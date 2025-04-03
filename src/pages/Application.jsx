e57f134171312b4b2498bfd1be4bed45549ca2ec
Prashant <prashantsherkar7515400@gmail.com>
2025-04-04 02:23:00 +0530

    adding remaining shit

--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -24,14 +24,13 @@ function Application() {
                         <h1></h1>
                     </div>
                     <div className="space-x-4 flex items-center">
-                        <a href="#features" className="text-green-700 hover:text-green-900">Features</a>
-                        <a href="#how-it-works" className="text-green-700 hover:text-green-900">How It Works</a>
                         <h1 className=''>Welcome back, Prashant</h1>
                     </div>
                 </nav>
             </div>
 
             <div className=' h-screen flex flex-col items-center justify-center gap-16 '>
+                <h1 className='flex items-center justify-center font-bold text-3xl gap-2' >Welcome to <Sprout className='text-green-500 scale-150'/><span className='text-green-500'>SmartKrishi</span></h1>
                 <div className='flex gap-3 flex-col items-center justify-center'>
                     <Input placeholder="Enter Crop name" value={crop} onChange={(e) => setCrop(e.target.value)} />
                     <Input placeholder="Enter City name" value={city} onChange={(e) => setCity(e.target.value)} />
